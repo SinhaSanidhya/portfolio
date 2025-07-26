871fc6daa05552a325c25ea019512882c062a8a5
Sanidhya Sinha <sanidhyasinha86@gmail.com>
2025-07-27 02:11:59 +0530

    refactor: improve HTML structure and CSS styling for better readability and layout

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // Simple slider logic
-const images = document.querySelectorAll('.slider img');
-        let current = 0;
-        function showImage(idx) {
-            images.forEach((img, i) => img.classList.toggle('active', i === idx));
-        }
-        document.querySelector('.prev').onclick = () => {
-            current = (current - 1 + images.length) % images.length;
-            showImage(current);
-        };
-        document.querySelector('.next').onclick = () => {
-            current = (current + 1) % images.length;
-            showImage(current);
-        };
-        setInterval(() => {
-            current = (current + 1) % images.length;
-            showImage(current);
-        }, 3000);
+const images = document.querySelectorAll(".slider img");
+let current = 0;
+function showImage(idx) {
+  images.forEach((img, i) => img.classList.toggle("active", i === idx));
+}
+document.querySelector(".prev").onclick = () => {
+  current = (current - 1 + images.length) % images.length;
+  showImage(current);
+};
+document.querySelector(".next").onclick = () => {
+  current = (current + 1) % images.length;
+  showImage(current);
+};
+setInterval(() => {
+  current = (current + 1) % images.length;
+  showImage(current);
+}, 3000);
